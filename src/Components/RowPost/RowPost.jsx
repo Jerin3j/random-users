cb566a6cbf7e1c5df363cd5569a5e2826223e51b
Jerin jj <jerinjj202@gmail.com>
2023-06-11 05:29:02 +0530

    one rowPost created

--- src/Components/RowPost/RowPost.jsx
+++ src/Components/RowPost/RowPost.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
+import { useEffect } from 'react'
 
 const RowPost = () => {
-  
+  useEffect(()=>{
+    async(()=>(
+const persons = "f"
+    ))
+  }, [])
   return (
     <div className='PersonRows'>
       <div className='Rows flex mx-72 mt-14 gap-3'>
-        <div className='Row flex flex-col border gap-2 px-10 py-6 rounded shadow-lg bg-white focus:bg-violet-500'>
-          <h1 className='Gender text-gray-600'>Female</h1>
-          <h1 className='Name text-lg font-semibold'>Mrs teke veetele shantha</h1>
-          <h1 className='text-red-500 active:text-white'>shantha@gmail.com</h1>
+        <div className='Row flex flex-col border gap-2 px-10 py-6 rounded-lg shadow-lg bg-white focus:bg-violet-500 cursor-pointer'>
+          <h1 className='Gender text-gray-600 bg-white'>Female</h1>
+          <h1 className='Name text-lg font-semibold bg-white'>Mrs teke veetele shantha</h1>
+          <h1 className='text-red-500 active:text-white bg-white'>shantha@gmail.com</h1>
         </div>
       </div>
     </div>
